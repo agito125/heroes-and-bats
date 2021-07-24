e2c74ac5d21e90d2eb34ce032f00d066a41b6df5
agito125 <66434838+agito125@users.noreply.github.com>
2021-07-24 14:02:48 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let enemy: Sprite = null
 let player = sprites.create(img`
     ........................
     .....ffff...............
@@ -23,4 +24,35 @@ let player = sprites.create(img`
     ........................
     ........................
     ........................
-`,SpriteKind.Player)
+    `, SpriteKind.Player)
+controller.moveSprite(player)
+game.onUpdateInterval(2000, function () {
+    enemy = sprites.create(img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c b b b b b b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b c b b b c b b b b f . . . . 
+        f b 1 f f f 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `, SpriteKind.Enemy)
+    enemy.setFlag(SpriteFlag.AutoDestroy, true)
+    enemy.setPosition(160, randint(0,120))
+    enemy.setVelocity(-50,0)
+})
+info.setLife(3)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(player: Sprite, enemy: Sprite) {
+enemy.destroy()
+    info.changeLifeBy(-1)
+
+    
+})
